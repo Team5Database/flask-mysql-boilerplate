dba26b92b147364add02db736337f1aa1a094254
Kailin <67965369+kailinxGitHub@users.noreply.github.com>
2024-04-19 22:48:30 -0700

    fix companies api endpoint

--- flask-app/src/companies/companies.py
+++ flask-app/src/companies/companies.py
@@ -10,8 +10,7 @@ def get_companies():
     data = cursor.fetchall()
     row_headers = [x[0] for x in cursor.description]
     json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
+    for row in data:
         json_data.append(dict(zip(row_headers, row)))
     the_response = make_response(jsonify(json_data))
     the_response.status_code = 200
