2c11f4f581df3c7256b66083cd24aa42dddeb819
Kailin <67965369+kailinxGitHub@users.noreply.github.com>
2024-04-20 21:28:58 -0700

    reply count for forum fix

--- flask-app/src/forum/forum.py
+++ flask-app/src/forum/forum.py
@@ -7,7 +7,7 @@ forum = Blueprint('forum', __name__)
 def get_forum():
     if request.method == 'GET':
         cursor = db.get_db().cursor()
-        cursor.execute('SELECT layoffs.posts.*, count(layoffs.posts.parent_post_id) as replies_count, layoffs.users.username, layoffs.layoffs.company_id, layoffs.layoffs.reason, layoffs.companies.name as company_name FROM layoffs.posts left join layoffs.posts as replies on layoffs.posts.id = replies.parent_post_id join layoffs.users on layoffs.posts.user_id = layoffs.users.id join layoffs.layoffs on posts.event_id = layoffs.id join layoffs.companies on layoffs.company_id = companies.id where layoffs.posts.parent_post_id IS NULL group by layoffs.posts.id order by layoffs.posts.updated_at')
+        cursor.execute('SELECT layoffs.posts.*, count(replies.id) as replies_count, layoffs.users.username, layoffs.layoffs.company_id, layoffs.layoffs.reason, layoffs.companies.name as company_name FROM layoffs.posts left join layoffs.posts as replies on layoffs.posts.id = replies.parent_post_id join layoffs.users on layoffs.posts.user_id = layoffs.users.id join layoffs.layoffs on posts.event_id = layoffs.id join layoffs.companies on layoffs.company_id = companies.id where layoffs.posts.parent_post_id IS NULL group by layoffs.posts.id order by layoffs.posts.updated_at DESC')
         row_headers = [x[0] for x in cursor.description]
         json_data = []
         theData = cursor.fetchall()
